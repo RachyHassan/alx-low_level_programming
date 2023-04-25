49d5f4af5a435c6441b5f9652eddf3784ec00dbf
RachyHassan <Tolulopehassan97@gmail.com>
2023-04-25 21:50:50 +0000

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * main- entry point
+  * print_times_table - Entry point
+  * description: 'print n times table'
+  * Return: always 0
+  */
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
+}
