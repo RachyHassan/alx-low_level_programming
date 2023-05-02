3728c16798e76467b0d51be8889074f9a1cfadee
RachyHassan <Tolulopehassan97@gmail.com>
2023-05-03 00:13:01 +0100

    generate passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  *main - generate passwords
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	int pass[100];
+	int a, b, c;
+
+	b = 0;
+
+	srand(time(NULL));
+
+	for (a = 0 a < 100; a++)
+	{
+		pass[a] = rand() % 78;
+		b += (pass[i] + '0');
+		putchar(pass[a] + '0');
+		if ((2772 - b) - '0' < 78)
+		{
+			c = 2772 - b - '0';
+			b += c;
+			putchar(c + '0');
+			break;
+		}
+	}
+	return (0);
+}
