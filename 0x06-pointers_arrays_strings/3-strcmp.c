e37b7762a8dd29c00399fc6c20ad72eae2efae85
RachyHassan <Tolulopehassan97@gmail.com>
2023-05-05 15:06:52 +0100

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,14 +5,14 @@
   * @s2: second value
   * retuen: comparison
   */
-int_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int j;
 
 	j = 0;
 	while (s1[j] != '\0' && s2[j] != '\0')
 	{
-		if (si[j] != s2[j])
+		if (s1[j] != s2[j])
 		{
 			return (s1[j] - s2[j]);
 		}
