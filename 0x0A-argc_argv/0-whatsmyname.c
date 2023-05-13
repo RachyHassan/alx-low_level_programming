514b388f1edbb7194b5702d371fda69d48b70d91
Rachy Hassan <RachyHassan@github.com>
2023-05-13 12:03:02 -0700

    my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,12 @@
 /**
  * main - prints name of a program
  * @argc: argument count
- * _attribute_((unused)): unused attribute
  * @argv: array of arguments
  * Return: always 0
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	(void) argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
