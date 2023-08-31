028f90686d9bc6ae2e3e93874b58f52a1505747a
RachyHassan <tolulopehassan97@gmail.com>
2023-08-31 14:56:44 +0100

    bit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * binary_to_unit - changes binary to integers
+ * @b - string
+ * Return: integer
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int p;
+	unsigned int int_val = 0;
+	
+	if (!b)
+		return (0);
+	for (p = 0; b[p]; p++)
+	{
+		if (b[p] < '0' || b[p] > '1')
+			return (0);
+		int_val = 2 * int_val + (b[p] - '0');
+	}
+	return (int_val);
+}
