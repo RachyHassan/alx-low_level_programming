421a7d15a85a9f2025a5acb862026dbc346a62d1
RachyHassan <tolulopehassan97@gmail.com>
2023-08-31 20:53:01 -0700

    bit'

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,7 +14,6 @@ void print_binary(unsigned long int n)
 	if (n == 0)
 	{
 		_putchar ('0');
-		return;
 	}
 
 	while (n > 0)
@@ -26,6 +25,5 @@ void print_binary(unsigned long int n)
 
 	for (j = m - 1; j >= 0; j--)
 	{
-		_putchar(binary_array[j] + '0');	}
-	return;
+		_putchar(binary_array[j] + '0');
 }
